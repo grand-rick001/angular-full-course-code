d6c1b12b648cd18b1fd6741ae5326782b078f619
Patrick Kabuga <kabugap02@gmail.com>
2023-06-08 04:46:13 +0300

    [feat]: Add new component

---
+++ Angular/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit{
+  hotelName: string = 'Kempinski';
+  numberOfRooms: number = 10;
+  hideRooms: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggle(): void {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}
