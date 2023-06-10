41980bfb4aceff9078a171bdf93d844acf6fd788
Patrick Kabuga <kabugap02@gmail.com>
2023-06-10 04:27:17 +0300

    [learn]: changeDetection strategy and devtools(render)

--- Angular/hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
+++ Angular/hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { RoomList } from '../room';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoomsListComponent implements OnInit {
   @Input({required: true}) rooms: RoomList[] = [];
+  @Output() selectedRoom = new EventEmitter<RoomList>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
 
 }
--- Angular/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ Angular/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -10,6 +10,7 @@ export class RoomsComponent implements OnInit{
   hotelName: string = 'Kempinski';
   numberOfRooms: number = 10;
   hideRooms: boolean = false;
+  selectedRoom!: RoomList;
 
   rooms: Room = {
     totalRooms: 20,
@@ -63,4 +64,22 @@ export class RoomsComponent implements OnInit{
     this.hideRooms = !this.hideRooms;
   }
 
+  selectRoom(room: RoomList) {
+    console.log(room);
+    this.selectedRoom = room;
+  }
+
+  addRoom(): void {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, BathRoom, Kitchen',
+      price: 5900,
+      photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2-21'),
+      rating: 4.5
+    }
+    this.roomList.push(room);
+  }
 }
