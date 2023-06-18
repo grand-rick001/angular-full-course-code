1d6ce6d20249f9b8966a70ab41e584124ab51382
Patrick Kabuga <kabugap02@gmail.com>
2023-06-19 01:06:31 +0300

    [learn]: dependency injection, nullInjectorError

--- Angular/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ Angular/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, DoCheck, ViewChild, ViewChildren, AfterViewInit, AfterViewChecked, QueryList } from '@angular/core';
 import { Room, RoomList } from './room';
 import { HeaderComponent } from 'src/app/header/header.component';
+import { RoomsService } from '../services/rooms.service';
 
 @Component({
   selector: 'app-rooms',
@@ -27,43 +28,12 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterView
 
   color: string = 'red';
 
-  constructor() { }
+  constructor(private roomsService: RoomsService) { }
 
   // Lifecycle hooks: ngOnInit -> Called when the component initializes
   ngOnInit(): void {
     // console.log(this.headerComponent+'HeaderComponent');
-    this.roomList = [
-      {
-        roomNumber: 1,
-        roomType: 'Deluxe Room',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, BathRoom, Kitchen',
-        price: 500,
-        photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-        checkinTime: new Date('11-Nov-2021'),
-        checkoutTime: new Date('12-Nov-2-21'),
-        rating: 4.5
-      },
-      {
-        roomNumber: 2,
-        roomType: 'Deluxe Room',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, BathRoom, Kitchen',
-        price: 2000,
-        photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-        checkinTime: new Date('11-Nov-2021'),
-        checkoutTime: new Date('12-Nov-2-21'),
-        rating: 3.4
-      },
-      {
-        roomNumber: 3,
-        roomType: 'Private Suite',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, BathRoom, Kitchen',
-        price: 1500,
-        photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-        checkinTime: new Date('11-Nov-2021'),
-        checkoutTime: new Date('12-Nov-2-21'),
-        rating: 2.6
-      }
-    ]
+    this.roomList = this.roomsService.getRooms();
   }
 
   ngDoCheck(): void {
