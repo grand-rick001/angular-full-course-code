05dc7636afbbabbd066f190e4755ca21cc83cb37
Patrick Kabuga <kabugap02@gmail.com>
2023-06-18 03:11:28 +0300

    [learn]: AfterContentInit, CContentChild, ng-content, select

--- Angular/hotelinventoryapp/src/app/container/container.component.ts
+++ Angular/hotelinventoryapp/src/app/container/container.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterContentInit, ContentChild } from '@angular/core';
 import { EmployeeComponent } from './employee/employee.component';
-import { RoomsComponent } from 'src/app/rooms/rooms.component';
+// import { RoomsComponent } from 'src/app/rooms/rooms.component';
 
 @Component({
   selector: 'app-container',
@@ -9,10 +9,10 @@ import { RoomsComponent } from 'src/app/rooms/rooms.component';
 })
 export class ContainerComponent implements AfterContentInit {
   @ContentChild(EmployeeComponent) employee!: EmployeeComponent;
-  @ContentChild(RoomsComponent) rooms!: RoomsComponent;
+  // @ContentChild(RoomsComponent) rooms!: RoomsComponent;
 
   ngAfterContentInit(): void {
-    console.log(this.rooms);
+    // console.log(this.rooms);
     console.log(this.employee);
     this.employee.name = 'David';
   }
