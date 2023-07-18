95a38c4a8c60710edca8c8cd6d59dd780ad80db7
Patrick Murimi <kabugap02@gmail.com>
2023-07-19 02:10:13 +0300

    [learn]: add 404 page styling

--- Angular/hotelinventoryapp/src/app/app-routing.module.ts
+++ Angular/hotelinventoryapp/src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
   { path: 'rooms', component: RoomsComponent },
   { path: 'rooms/:id', component: RoomsBookingComponent },
-  { path: '**', component: NotfoundComponent },
-  { path: '', redirectTo: '/rooms', pathMatch: 'full' }
+  { path: '', redirectTo: '/rooms', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
