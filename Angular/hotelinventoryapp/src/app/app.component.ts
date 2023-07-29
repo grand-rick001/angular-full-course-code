1cc9ae0c2783d054b9e197cf0708131a7706f185
Patrick Murimi <kabugap02@gmail.com>
2023-07-29 20:55:43 +0300

    [learn]: Understand how the route events work

--- Angular/hotelinventoryapp/src/app/app.component.ts
+++ Angular/hotelinventoryapp/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { LoggerService } from 'src/app/services/logger.service';
 import { localStorageToken } from 'src/app/token/localstorage.token';
 import { InitService } from 'src/app/services/init.service';
 import { ConfigService } from './services/config.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +20,8 @@ export class AppComponent implements OnInit {
     @Optional() private loggerService: LoggerService,
     @Inject(localStorageToken) private localStorageToken: Storage,
     private initService: InitService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private router: Router
     ) {
       console.log(this.initService.config);
     }
@@ -27,8 +30,17 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.loggerService?.log('AppComponent.ngOnInit()');
     // this.name.nativeElement.innerText = "Hilton Hotels";
-
     this.localStorageToken.setItem('name', 'Hilton Hotel');
+    // this.router.events.subscribe(event => {
+    //   console.log(event);
+    // })
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    ).subscribe(event => console.log('Navigation Started'))
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe(event => console.log('Navigation Completed'))
   }
 
   // ngAfterViewInit(): void {
